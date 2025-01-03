e4d8b9f417ca5adb857fa10b495573d5e13fdbcb
Helior Colorado <me@helior.info>
2025-01-02 18:58:14 -0800

    Initial commit

---
+++ test/ab3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ab3 from '../lib/ab3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ab3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ab3.Ab3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
